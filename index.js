7988490a3c3e17a6fbcde30dde2ec33f90adc2ba
Max Snowden <mxsnowden100@gmail.com>
2022-07-17 12:22:33 +0100

    Removed unused import

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-const { response } = require('express')
 const express = require('express')
 const app = express()
 app.set('view engine', 'ejs')
 app.listen(8080, function () {
     console.log('Application Started')
 })
-app.get('/?', function (req, res) {
+app.get('/home', function (req, res) {
     console.log('i');
     res.render('index', {
         num: 5
